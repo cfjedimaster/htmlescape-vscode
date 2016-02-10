3de9f5b0b1520c08fe54a2b7723ab993dee0e210
Raymond Camden <raymondcamden@gmail.com>
2016-02-10 08:47:52 -0600

    initial

---
+++ README.md
@@ -0,0 +1,19 @@
+# HTML Escape Extension
+
+This is a Visual Studio Code version of my older Brackets extension. It simply converts HTML (and CSS, JS) into web-ready text that can be used in a blog entry. So for example, a &lt; character will become &amp;lt;. 
+
+This extension is a modified version of a sample created by Erich Gamma of the Visual Studio team. You
+can find his sample here: 
+
+https://github.com/Microsoft/vscode-extension-samples/tree/master/textdocumentprovider-sample
+
+I want to give him, and the rest of the VSC team, big thanks for both adding the feature required
+to make this extension work and for building a sample I could use to build this. (To be clear, 95%
+of the code here is his - I just modified what I need to!)
+
+To use, simply open a file and run `Show Escaped HTML`. If no text is selected, it will convert 
+the entire document.
+
+![Example](images/preview.gif)
+
+
---
+++ src/extension.ts
@@ -0,0 +1,102 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    let previewUri = vscode.Uri.parse('html-escape://cfjedimaster/html-escape');
+
+    class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
+        private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+
+        public provideTextDocumentContent(uri: vscode.Uri): string {
+            return this.createEscapedHtml();
+        }
+
+        get onDidChange(): vscode.Event<vscode.Uri> {
+            return this._onDidChange.event;
+        }
+
+        public update(uri: vscode.Uri) {
+            this._onDidChange.fire(uri);
+        }
+
+        private createEscapedHtml() {
+			console.log('createEscapedHtml');
+            let editor = vscode.window.activeTextEditor;
+			/*
+            if (!(editor.document.languageId === 'css')) {
+                return this.errorSnippet("Active editor doesn't show a CSS document - no properties to preview.")
+            }
+			*/
+			console.log(this.extractSnippet());
+            return this.extractSnippet();
+        }
+
+        private extractSnippet(): string {
+            let editor = vscode.window.activeTextEditor;
+			//if we have a selection, use that, otherwise, whole document
+			console.log('is it empty? '+editor.selection.isEmpty);
+			if(editor.selection.isEmpty) {
+	            return this.snippet(editor.document.getText());
+			} else {
+				console.log('return selection');
+				//there must be a better way of doing this
+	            let selStart = editor.document.offsetAt(editor.selection.anchor);
+				let selEnd = editor.document.offsetAt(editor.selection.end);
+				return this.snippet(editor.document.getText().slice(selStart,selEnd));
+			}
+        }
+
+        private errorSnippet(error: string): string {
+            return `
+                <body>
+                    ${error}
+                </body>`;
+        }
+
+        private snippet(str): string {
+
+            str = str.replace(/&/g, "&amp;");
+            str = str.replace(/</g, "&lt;");
+			console.log('ok, wtf, i should have esvaped html now '+str);
+            str = str.replace(/>/g, "&gt;");
+            str = str.replace(/"/g, "&quot;");
+            str = str.replace(/'/g, "&#x27;");
+            str = str.replace(/\//g, "&#x2F;");
+			
+			//Required since we're rendering in HTML itself...
+			str = str.replace(/&/g, "&amp;");
+			
+			return '<body><h1>Escaped HTML</h1>' + str;
+        }
+    }
+
+    let provider = new TextDocumentContentProvider();
+    let registration = vscode.workspace.registerTextDocumentContentProvider('html-escape', provider);
+
+    vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
+        if (e.document === vscode.window.activeTextEditor.document) {
+            provider.update(previewUri);
+        }
+    });
+
+    vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+        if (e.textEditor === vscode.window.activeTextEditor) {
+            provider.update(previewUri);
+        }
+    })
+
+    let disposable = vscode.commands.registerCommand('extension.showHtmlEscape', () => {
+		console.log('running dis');
+        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two).then((success) => {
+        }, (reason) => {
+            vscode.window.showErrorMessage(reason);
+        });
+
+    });
+    context.subscriptions.push(disposable, registration);
+}
+
+export function deactivate() {
+}
